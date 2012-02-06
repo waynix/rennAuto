babc97511729295d9578beb72d0ae6580d1965d7
waynix <github@klappezu.org>
2012-02-06 15:00:14 +0100

    Corrected Config

--- TimerTest.c
+++ TimerTest.c
@@ -11,8 +11,8 @@ int main()
 {
 	DDRD = (1 << DD6);
 	PORTD = (0 << DD6);
-	TCCR1A = (0 << WGM10) || (1 << WGM11);
-	TCCR1B = (0 << WGM13) || (0 << WGM12) || (0 << CS10) || (1 << CS11) || (0 << CS12); //
+	TCCR1A = (0 << WGM10) || (0 << WGM11); //WGM10 - 13 ctc
+	TCCR1B = (0 << WGM13) || (1 << WGM12) || (1 << CS10) || (1 << CS11) || (0 << CS12); // divide system clock by 64
 	TIMSK1 = (1 << OCIE1A);
 	OCR1A = 5000;//diese aktion ist nicht atomar(16bit) und greif auf timersachen zu deswegen die interrupts erst danach setzen
 	sei();
