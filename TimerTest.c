38c336ca16b56345394eee48ae4edbe5192dbd47
Paul Rosset <paul@r0sset.com>
2012-02-06 23:17:10 +0100

    fixed bug that used bytewise instead  of bitwise operations Oo damn

--- TimerTest.c
+++ TimerTest.c
@@ -7,25 +7,30 @@
  */
 #import <avr/io.h>
 #import <avr/interrupt.h>
-int main()
+
+int main(int argc, char** argv)
 {
-	DDRD = (1 << DD6);
-	PORTD = (0 << DD6);
-	TCCR1A = (0 << WGM10) || (0 << WGM11); //WGM10 - 13 ctc
-	TCCR1B = (0 << WGM13) || (1 << WGM12) || (1 << CS10) || (1 << CS11) || (0 << CS12); // divide system clock by 64
+	DDRD |= (1 << DDD7) | (1 << DDD5);
+	PORTD |= (1 << PD7) | (1 << PD5);
+
+	TCCR1A = (0 << WGM10) | (0 << WGM11); //WGM10 - 13 ctc
+	TCCR1B = (0 << WGM13) | (1 << WGM12) | (0 << CS12) | (1 << CS11) | (1 << CS10); // divide system clock by 64
 	TIMSK1 = (1 << OCIE1A);
-	OCR1A = 5000;//diese aktion ist nicht atomar(16bit) und greif auf timersachen zu deswegen die interrupts erst danach setzen
+	OCR1A = 5000;
+
 	sei();
 
-	while(1){}
+	while(1){/*do not finish*/}
 	return 0;
 }
+ISR(TIMER1_OVF_vect){}
 ISR(TIMER1_COMPA_vect)
 {
-    PORTD ^= (1 << DD6); //toggle pin 6
-//    char tmp = SREG; //backup SREG state
-//    cli();
-//    //do something that ist not atomic
-//    SREG = tmp;//Restore SREG
+    PORTD ^= (1 << DD7); //toggle pin 6
+    char tmp = SREG; //backup SREG state
+    cli();
+    //do something that ist not atomic
+
+    SREG = tmp;//Restore SREG
 }
 
