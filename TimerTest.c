58ae921004015910c5c03c38867635e151479f34
Paul Rosset <paul@r0sset.com>
2012-02-07 00:27:20 +0100

    Servo implemented

--- TimerTest.c
+++ TimerTest.c
@@ -7,7 +7,10 @@
  */
 #import <avr/io.h>
 #import <avr/interrupt.h>
-
+uint8_t servo1;
+uint8_t nextServo;
+uint16_t usedTime;
+#define SERVOCOUNT 1
 int main(int argc, char** argv)
 {
 	DDRD |= (1 << DDD7) | (1 << DDD5);
@@ -17,7 +20,8 @@ int main(int argc, char** argv)
 	TCCR1B = (0 << WGM13) | (1 << WGM12) | (0 << CS12) | (1 << CS11) | (1 << CS10); // divide system clock by 64
 	TIMSK1 = (1 << OCIE1A);
 	OCR1A = 5000;
-
+	usedTime = 0;
+	servo1 = 0;
 	sei();
 
 	while(1){/*do not finish*/}
@@ -26,10 +30,28 @@ int main(int argc, char** argv)
 ISR(TIMER1_OVF_vect){}
 ISR(TIMER1_COMPA_vect)
 {
-    PORTD ^= (1 << DD7); //toggle pin 6
     char tmp = SREG; //backup SREG state
     cli();
     //do something that ist not atomic
+    //PORTD ^= (1 << DD7); //toggle pin 7
+    switch(nextServo)
+    {
+    case 0:
+    	PORTD |= (1 << PD7);
+    	OCR1A = 0x100| servo1;
+    	break;
+    case 1:
+    	PORTD &= ~(1 << PD7);
+    	OCR1A = 5000 - usedTime;
+
+    }
+    usedTime += OCR1A;
+    nextServo++;
+    if(nextServo > SERVOCOUNT)
+    {
+    	usedTime = 0;
+    	nextServo = 0;
+    }
 
     SREG = tmp;//Restore SREG
 }
